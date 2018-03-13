6d1df256856ab51ba720d726d4bcbfea9453a34b
christopherwamble <christopher.wamble@canary.is>
2018-03-13 12:22:50 -0400

    uploading the program.

--- AstronautSays/ViewController.swift
+++ AstronautSays/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var astronautImg: UIImageView!
+    @IBOutlet weak var astronautGreeting: UIImageView!
+    @IBOutlet weak var resetButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        actionButton.isHidden = false
+        astronautImg.isHidden = true
+        astronautGreeting.isHidden = true
+        resetButton.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func astronautAction(_ sender: Any) {
+        actionButton.isHidden = true
+        astronautImg.isHidden = false
+        astronautGreeting.isHidden = false
+        resetButton.isHidden = false
+    }
+    
+    @IBAction func resetAction(_ sender: Any) {
+        actionButton.isHidden = false
+        astronautImg.isHidden = true
+        astronautGreeting.isHidden = true
+        resetButton.isHidden = true
+    }
+    
 }
 
